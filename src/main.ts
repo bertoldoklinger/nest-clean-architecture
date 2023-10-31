959f6544f9b014f282a56edecfa461dbf9b4677f
Bertoldo Klinger <bertoldokbrj@gmail.com>
2023-10-31 11:14:15 -0300

    feat: add config module env

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,16 @@
+import { ConfigService } from '@nestjs/config'
 import { NestFactory } from '@nestjs/core'
 import { AppModule } from './app.module'
+import { Env } from './env'
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, {
     // logger: false,
   })
-  await app.listen(3000)
+
+  const configService: ConfigService<Env, true> = app.get(ConfigService)
+  const port = configService.get('PORT', { infer: true })
+
+  await app.listen(port)
 }
 bootstrap()
